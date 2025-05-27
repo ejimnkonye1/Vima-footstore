5012e4e051528242192b4e10b91b35068146dead
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2025-05-28 00:57:22 +0100

    deleted

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,9 @@
 
 import { useState, useEffect } from 'react';
 import './App.css';
-import Header from './home/header.jsx';
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import ProductDetails from './component/productdetails';
-import Men from './component/men';
-import Woman from './component/woman';
-import Catalog from './component/catalog';
-
-import Login from './auth/login.jsx';
-
-import Cart from './component/cart';
-
-import Checkout from './payment/checkout.jsx';
 import WhatsAppLink from './util/whatsapp.jsx';
-
-import SearchPage from './component/searchpg';
-
-import SimpleSlider from './home/home.jsx';
-// import AutoPlay from './component/autoplayproduct';
-import { auth } from "./Firebase"; 
-import Recommend from './component/recommended';
-import BillingAddressForm from './payment/checkout.jsx';
-import ConfirmationPage from './payment/confirm.jsx';
-
-import OrderPage from './profile/order';
 import ContactInfo from './util/contactInfo.jsx';
-import ProductForm from './home/add.jsx';
 import ECommerceStore from './test.jsx';
 import Footer from './util/footer.jsx';
 import ProductDetailsNew from './new/productdetails.jsx';
@@ -53,6 +28,7 @@ const Loader = () => (
 
 
 // Layout component that includes the loader logic
+
 const Layout = ({ children }) => {
   const [isLoading, setIsLoading] = useState(true);
 
@@ -68,13 +44,6 @@ const Layout = ({ children }) => {
 };
 
 function App() {
-  const [formData, setFormData] = useState({});
-const handleFormDataChange = (newFormData) => {
-  setFormData(newFormData);
-};
-  const [cartItems, setCartItems] = useState([]); // Define cart state
-  const [searchError, setSearchError] = useState(false); // State for search error
-  // Add this line at the beginning of your component
 
 
   return (
@@ -85,18 +54,6 @@ const handleFormDataChange = (newFormData) => {
   <Layout>
 
     <Routes>
-      <Route path='/sli' element={<SimpleSlider cartItems={cartItems} setCartItems={setCartItems} />} />
-
-      <Route path='/confirm' element={<ConfirmationPage cartItems={cartItems} setCartItems={setCartItems} />} />
-      <Route path='/order' element={<OrderPage formData={formData} />} />
-
-
-  
-      {/* <Route path='/auto' element={<AutoPlay cartItems={cartItems} setCartItems={setCartItems} />} /> */}
-      <Route path='/recommend' element={<Recommend cartItems={cartItems} setCartItems={setCartItems} />} />
-      <Route path="/searchpg/:query" element={<SearchPage searchError={searchError} />} />
-
-            <Route path='/add' element={<ProductForm/>} />
                <Route path='/' element={<ECommerceStore />} />
                               <Route path='/product/:name' element={<ProductDetailsNew />} />
                                                             <Route path='/cart' element={<CartPage />} />
