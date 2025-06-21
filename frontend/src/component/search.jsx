f0522849c0d3b5275a00cefd74e9d81006063a57
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2025-06-21 20:29:07 +0100

    new

--- frontend/src/component/search.jsx
+++ frontend/src/component/search.jsx
@@ -2,7 +2,7 @@ import { FiSearch } from "react-icons/fi";
 
 const Search = ({ searchQuery, setSearchQuery }) => {
   return (
-    <div className="mt-4 w-full">
+    <div className="md:hidden mt-4 w-full">
       <header className="">
         <div className="container mx-auto px-4 py-4 w-full">
           <div className="flex items-center">
--- frontend/src/util/bottomnav.jsx
+++ frontend/src/util/bottomnav.jsx
@@ -1,16 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { FaHome, FaStore, FaShoppingCart, FaUser } from "react-icons/fa";
 
 function BottomNavbar() {
+  const location = useLocation();
+
   return (
-    <div className="lg:hidden fixed bottom-0  left-0 right-0 bg-white shadow-md z-50">
+    <div className="lg:hidden fixed bottom-0 left-0 right-0 bg-white shadow-md z-50">
       <div className="border-t border-gray-200">
         <ul className="flex justify-around items-center py-2">
           <li className="flex-1 text-center">
             <Link 
               to="/" 
-              className="flex flex-col items-center justify-center p-2 text-gray-700 hover:text-blue-500 transition-colors"
+              className={`flex flex-col items-center justify-center p-2 ${location.pathname === '/' ? 'text-blue-600' : 'text-gray-500'}`}
             >
               <FaHome className="text-xl" />
               <span className="text-xs mt-1">Home</span>
@@ -18,8 +20,8 @@ function BottomNavbar() {
           </li>
           <li className="flex-1 text-center">
             <Link 
-              to="/Cat" 
-              className="flex flex-col items-center justify-center p-2 text-gray-700 hover:text-blue-500 transition-colors"
+              to="/" 
+              className={`flex flex-col items-center justify-center p-2 ${location.pathname === '/shop' ? 'text-blue-600' : 'text-gray-500'}`}
             >
               <FaStore className="text-xl" />
               <span className="text-xs mt-1">Shop</span>
@@ -28,16 +30,10 @@ function BottomNavbar() {
           <li className="flex-1 text-center">
             <Link 
               to="/cart" 
-              className="flex flex-col items-center justify-center p-2 text-gray-700 hover:text-blue-500 transition-colors"
+              className={`flex flex-col items-center justify-center p-2 ${location.pathname === '/cart' ? 'text-blue-600' : 'text-gray-500'}`}
             >
               <div className="relative">
                 <FaShoppingCart className="text-xl" />
-                {/* Uncomment if you want to show cart count */}
-                {/* {cartCount > 0 && (
-                  <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
-                    {cartCount}
-                  </span>
-                )} */}
               </div>
               <span className="text-xs mt-1">Cart</span>
             </Link>
@@ -45,7 +41,7 @@ function BottomNavbar() {
           <li className="flex-1 text-center">
             <Link 
               to="/login" 
-              className="flex flex-col items-center justify-center p-2 text-gray-700 hover:text-blue-500 transition-colors"
+              className={`flex flex-col items-center justify-center p-2 ${location.pathname === '/login' ? 'text-blue-600' : 'text-gray-500'}`}
             >
               <FaUser className="text-xl" />
               <span className="text-xs mt-1">Account</span>
